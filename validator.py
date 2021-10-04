a3323c293a99ce39ab13a32ce71e221f24b35bcd
arthur-david <arthurdavid000@gmail.com>
2021-10-04 16:47:06 -0300

    Refactoring Validator

--- validator.py
+++ validator.py
@@ -8,22 +8,18 @@ def validator(cpf):
     prototype_cpf += _calculator_cpf(prototype_cpf)
     prototype_cpf += _calculator_cpf(prototype_cpf)
 
-    prototype_cpf = prototype_cpf[:3] + "." + prototype_cpf[3:6] + "." + prototype_cpf[6:9] + "-" + prototype_cpf[9:]
+    new_cpf = prototype_cpf[:3] + "." + prototype_cpf[3:6] + "." + prototype_cpf[6:9] + "-" + prototype_cpf[9:]
 
     print(f'\nValidating {cpf}\n...')
-    print("Valid CPF!" if cpf == prototype_cpf else f"Invalid CPF!")
+    print("Valid CPF!" if cpf == new_cpf else f"Invalid CPF!")
 
 
 def _calculator_cpf(cpf):
     counts = []
-    if len(cpf) > 9:
-        for i, v in enumerate(range(11, 1, -1)):
-            num = int(cpf[i]) * v
-            counts.append(num)
-    else:
-        for i, v in enumerate(range(10, 1, -1)):
-            num = int(cpf[i]) * v
-            counts.append(num)
+    start = 11 if len(cpf) > 9 else 10
+    for i, v in enumerate(range(start, 1, -1)):
+        num = int(cpf[i]) * v
+        counts.append(num)
 
     total = sum(counts)
     num_generated = 11 - (total % 11)
